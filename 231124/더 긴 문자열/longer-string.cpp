24fb3eaf1f9938afaa445b43bac9834f436c2cc4
godzz733 <godzz733@naver.com>
2023-11-24 10:48:08 +0900

    Title: 더 긴 문자열, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 231124/더 긴 문자열/longer-string.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+int main() {
+    // 여기에 코드를 작성해주세요.
+    string a,b;
+    cin >> a >> b;
+    if (a.length() < b.length()) {
+        cout << b << " " << b.length();
+    } else if (a.length() > b.length()) {
+        cout << a << " " << a.length();
+    } else {
+        cout << "same";
+    }
+    return 0;
+}
