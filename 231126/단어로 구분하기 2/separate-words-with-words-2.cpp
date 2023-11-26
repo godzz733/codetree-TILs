5368d54eab5cd6e526f98f0b0e5280a4d0c11283
godzz733 <godzz733@naver.com>
2023-11-26 20:26:34 +0900

    Title: 단어로 구분하기 2, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 231126/단어로 구분하기 2/separate-words-with-words-2.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main() {
+    // 여기에 코드를 작성해주세요.
+    string tem;
+    for (int i=0; i<10; i++){
+        cin >> tem;
+        if (i&1) cout << tem << '\n';
+    }
+    return 0;
+}
