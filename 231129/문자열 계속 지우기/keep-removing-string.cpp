5b739fb005a2fa519dd5dfc8e10d70086c0ea967
godzz733 <godzz733@naver.com>
2023-11-29 15:51:57 +0900

    Title: 문자열 계속 지우기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 231129/문자열 계속 지우기/keep-removing-string.cpp
+++ 231129/문자열 계속 지우기/keep-removing-string.cpp
@@ -5,17 +5,9 @@ int main() {
     // 여기에 코드를 작성해주세요.
     string a,b;
     cin >> a >> b;
-    for (int i=0; i<100; i++){
-        if (a.length() < b.length()) break;
-        for (int j=0; j<a.length()-b.length()+1; j++){
-            if (a.substr(j,b.length()) == b) {
-                a.erase(j,b.length());
-                break;
-            }
-        }
-    }
-    if (a == b) {
-        a = "";
+    while (a.find(b) != string::npos) {
+        int idx = a.find(b);
+        a.erase(idx, b.length());
     }
     cout << a;
     return 0;
