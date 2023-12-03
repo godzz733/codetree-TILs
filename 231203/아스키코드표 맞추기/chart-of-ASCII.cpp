08283951615943a2b62001b8ee29748f5800c7ef
godzz733 <godzz733@naver.com>
2023-12-03 14:53:29 +0900

    Title: 아스키코드표 맞추기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 231203/아스키코드표 맞추기/chart-of-ASCII.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int tem;
+    for (int i=0; i<5;i++){
+        cin >> tem;
+        cout << (char)tem << " ";
+    }
+    return 0;
+}
