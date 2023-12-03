44188bb3fc5e50a0e86dc5b0c8756722fb30011f
godzz733 <godzz733@naver.com>
2023-12-03 21:49:13 +0900

    Title: 반복 출력하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 231203/반복 출력하기/repeated-output.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+void A() {
+    std::cout << "12345^&*()_" << '\n';
+}
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n;
+    std::cin >> n;
+    for (int i=0; i<n; i++) A();
+    return 0;
+}
