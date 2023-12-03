bf46d6097a95d43d4e667af56f8140abeea2e7d7
godzz733 <godzz733@naver.com>
2023-12-03 14:56:19 +0900

    Title: 그 다음 알파벳, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 231203/그 다음 알파벳/next-alphabet.cpp
+++ 231203/그 다음 알파벳/next-alphabet.cpp
@@ -4,6 +4,10 @@ int main() {
     // 여기에 코드를 작성해주세요.
     char a;
     std::cin >> a;
+    if (a=='z'){
+        std::cout << 'a';
+        return 0;
+    }
     std::cout << (char) ((int)a + 1);
     return 0;
 }
