6bc4d2cdb43f6355541ae44fdc12426251b93213
godzz733 <godzz733@naver.com>
2024-04-14 21:35:31 +0900

    Title: 코드트리 투어, Time: 1113ms, Memory: 30MB, Status: Wrong Answer - CodeTree

---
+++ 240414/코드트리 투어/codetree-tour.py
@@ -0,0 +1,54 @@
+import sys; input = sys.stdin.readline
+import heapq as h
+command = int(input())
+tem = list(map(int, input().split()))[1:]
+n, m = tem[0], tem[1]
+arr = [[] for _ in range(n+1)]
+for i in range(2,m*3,3):
+    a,b,c = tem[i],tem[i+1],tem[i+2]
+    arr[a].append((b,c))
+    arr[b].append((a,c))
+def dik(start):
+    d = [int(1e9)]*(n+1)
+    d[start] = 0
+    q = [(0,start)]
+    while q:
+        dist, now = h.heappop(q)
+        if d[now] < dist:
+            continue
+        for i in arr[now]:
+            cost = dist + i[1]
+            if cost < d[i[0]]:
+                d[i[0]] = cost
+                h.heappush(q, (cost, i[0]))
+    return d
+d = dik(0)
+del_arr = set()
+now = 0
+_map = []
+for _ in range(command-1):
+    com = list(map(int, input().split()))
+    if com[0] == 200:
+        h.heappush(_map,((-(com[2]- d[com[3]])),com[1],com[3],com[2]))
+    elif com[0] == 400:
+        while _map:
+            tem = h.heappop(_map)
+            if tem[1] in del_arr:
+                continue
+            if -tem[0] < 0:
+                print(-1)
+                h.heappush(_map,tem)
+                break
+            else:
+                print(tem[1])
+                break
+        else:
+            print(-1)
+    elif com[0] == 300:
+        del_arr.add(com[1])
+    else:
+        d = dik(com[1])
+        for i in range(len(_map)):
+            a,b,c,e = _map[i]
+            _map[i] = (-(e-d[c]),b,c,e)
+        h.heapify(_map)
