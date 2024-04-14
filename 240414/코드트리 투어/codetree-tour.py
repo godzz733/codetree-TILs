15c5d0b8064e5556a9024979de558f2d1e860e56
godzz733 <godzz733@naver.com>
2024-04-14 22:13:41 +0900

    Title: 코드트리 투어, Time: 781ms, Memory: 31MB, Status: Wrong Answer - CodeTree

--- 240414/코드트리 투어/codetree-tour.py
+++ 240414/코드트리 투어/codetree-tour.py
@@ -31,8 +31,8 @@ for _ in range(command-1):
     com = list(map(int, input().split()))
     if com[0] == 200:
         cost = com[2] - d[com[3]]
-        if d[com[3]] >= int(1e11):
-            h.heappush(_map,(int(1e11),com[1],com[3],com[2]))
+        if d[com[3]] >= int(1e11) or cost < 0:
+            h.heappush(_map,(100,com[1],com[3],com[2]))
         else:
             h.heappush(_map,(-cost,com[1],com[3],com[2]))
     elif com[0] == 400:
@@ -40,7 +40,7 @@ for _ in range(command-1):
             tem = h.heappop(_map)
             if tem[1] in del_arr:
                 continue
-            if -tem[0] < 0 or tem[0] >= int(1e11):
+            if -tem[0] < 0:
                 print(-1)
                 h.heappush(_map,tem)
                 break
@@ -59,8 +59,8 @@ for _ in range(command-1):
             if b in del_arr:
                 continue
             cost = e - d[c]
-            if d[c] >= int(1e11):
-                h.heappush(tem,(int(1e11),b,c,e))
+            if d[c] >= int(1e11) or cost < 0:
+                h.heappush(tem,(100,b,c,e))
             else:
                 h.heappush(tem,(-cost,b,c,e))
         _map = tem
